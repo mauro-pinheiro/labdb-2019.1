3af9cc4fbd2d4a307ec98448a5a49c872436e1d4
mauro <mauro.pinheiro@acad.ifma.edu.br>
2019-05-11 08:31:25 -0300

    Initial commit

---
+++ src/main/java/ifma/edu/mauro/lbbd/lp3/dao/ClienteDAO.java
@@ -0,0 +1,54 @@
+package ifma.edu.mauro.lbbd.lp3.dao;
+
+import ifma.edu.mauro.lbbd.lp3.entidades.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ClienteDAO implements DAO<Cliente> {
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public ClienteDAO(){
+
+    }
+
+    @Override
+    public Optional<Cliente> get(long id) {
+        return Optional.ofNullable(clientes.get((int)id));
+    }
+
+    @Override
+    public List<Cliente> getAll() {
+        return clientes;
+    }
+
+    @Override
+    public void save(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    @Override
+    public void update(Cliente cliente, String[] params) {
+        int i = clientes.indexOf(cliente);
+        if(i != -1) {
+            cliente = clientes.get(i);
+        } else {
+            clientes.add(cliente);
+        }
+        cliente.setNome(Objects.requireNonNull(
+                params[0], "Nome não pode ser nulo."));
+        cliente.setEndereco(Objects.requireNonNull(
+                params[1], "Endereco não pode ser nulo."));
+        cliente.setTelefone(Objects.requireNonNull(
+                params[2], "Telefone nao pode ser nulo."));
+    }
+
+    @Override
+    public void delete(Cliente cliente) {
+        clientes.remove(cliente);
+    }
+
+
+}
---
+++ src/main/java/ifma/edu/mauro/lbbd/lp3/entidades/Frete.java
@@ -0,0 +1,98 @@
+package ifma.edu.mauro.lbbd.lp3.entidades;
+
+import java.util.Objects;
+
+public class Frete {
+    private int codigo_frete;
+    private Cidade cidade;
+    private Cliente cliente;
+    private String descricao;
+    private float peso;
+    private float valor;
+
+    public Frete(Cidade cidade, Cliente cliente, String descricao, float peso, float valor) {
+        this.cidade = cidade;
+        this.cliente = cliente;
+        this.descricao = descricao;
+        this.peso = peso;
+        this.valor = valor;
+    }
+
+    public int getCodigo_frete() {
+        return codigo_frete;
+    }
+
+    public void setCodigo_frete(int codigo_frete) {
+        this.codigo_frete = codigo_frete;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Frete{" +
+                "codigo_frete=" + codigo_frete +
+                ", cidade=" + cidade +
+                ", cliente=" + cliente +
+                ", descricao='" + descricao + '\'' +
+                ", peso=" + peso +
+                ", valor=" + valor +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Frete)) return false;
+        Frete frete = (Frete) o;
+        return getCodigo_frete() == frete.getCodigo_frete() &&
+                Float.compare(frete.getPeso(), getPeso()) == 0 &&
+                Float.compare(frete.getValor(), getValor()) == 0 &&
+                Objects.equals(getCidade(), frete.getCidade()) &&
+                Objects.equals(getCliente(), frete.getCliente()) &&
+                Objects.equals(getDescricao(), frete.getDescricao());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo_frete(), getCidade(), getCliente(), getDescricao(), getPeso(), getValor());
+    }
+}

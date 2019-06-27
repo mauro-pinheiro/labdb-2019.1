d1936b9ba743b1c30d1b9e403111951201b559dc
Mauro Pinheiro <mauro.pinheiro@acad.ifma.edu.br>
2019-06-27 06:21:04 -0300

    Mapeamento Objeto Relacional completo

--- Lab05/Maven/XYZRentalCars/src/main/java/lab5/xyzrentalcars/entidades/Cliente.java
+++ Lab05/Maven/XYZRentalCars/src/main/java/lab5/xyzrentalcars/entidades/Cliente.java
@@ -99,7 +99,6 @@ public class Cliente {
                 ", numeroCNH='" + numeroCNH + '\'' +
                 ", validadeCNH=" + validadeCNH +
                 ", categriaCNH='" + categriaCNH + '\'' +
-                ", historicoReservas=" + historicoReservas +
                 '}';
     }
 

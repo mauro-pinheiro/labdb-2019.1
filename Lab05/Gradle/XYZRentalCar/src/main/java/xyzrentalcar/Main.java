662a12e2ca9b50412db6ce471d373f2f2c04bc18
Mauro Pinheiro <mauro.pinheiro@acad.ifma.edu.br>
2019-07-20 19:12:11 -0300

    XYZRentalCar -> DAOGenerico e crud basico implementado

--- Lab05/Gradle/XYZRentalCar/src/main/java/xyzrentalcar/Main.java
+++
@@ -1,9 +0,0 @@
-package xyzrentalcar;
-
-import javax.persistence.Persistence;
-
-public class Main{
-    public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("rentalcars");
-    }
-}
